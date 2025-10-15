f370560593755c14cbe05deb6e51da65dd756b55
Mohan Gupta <mohangupta@live.com>
2025-10-15 13:42:30 +1100

    compiles but can;'t find dll

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     // Create an instance of the AppWindow component
-    auto main_window = AppWindow::create().unwrap();
+    auto main_window = AppWindow::create();
 
     // Set the property defined in the .slint file
     main_window->set_greeting("Hello from Zig + C++ + Slint!");
