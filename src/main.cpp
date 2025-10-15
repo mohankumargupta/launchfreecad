f9674a2ed0f59336b9afa0c3dc6f7f23fc05a277
Mohan Gupta <mohangupta@live.com>
2025-10-15 17:17:36 +1100

    can call zig function from C++

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "generated.h"
+#include <vector>
 
 struct StringArray {
     const char** strings;
@@ -7,7 +8,7 @@ struct StringArray {
 
 extern "C" {
     StringArray freecad_folders();
-    void free_string_array(StringArray array);
+    void free_folders(StringArray array);
 }
 
 int main()
@@ -15,7 +16,20 @@ int main()
     auto main_window = AppWindow::create();
 
     StringArray folders_from_zig = freecad_folders();
+    std::vector<slint::SharedString> folder_vec;
+    for (size_t i = 0; i < folders_from_zig.len; ++i) {
+        folder_vec.push_back(folders_from_zig.strings[i]);
+    }
 
+    // 5. Create a Slint model from the C++ vector. This model will be
+    //    used by the ComboBox in your UI.
+    auto folder_model = std::make_shared<slint::VectorModel<slint::SharedString>>(folder_vec);
+
+    // 6. Set the `freecads` property in your Slint component with the new model.
+    main_window->set_freecads(folder_model);
+
+    // 7. IMPORTANT: Free the memory that was allocated by Zig to avoid memory leaks.
+    free_folders(folders_from_zig);
     //Run
     main_window->run();
 
